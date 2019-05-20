58f885a7361c25bf87e7cb00a7a95fc86525e728
Yan Grunenberger <yan@grunenberger.net>
2019-05-20 16:51:42 +0200

    Adding a mini NAT inside the docker and map the SSH port

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:latest
 MAINTAINER Yan Grunenberger <yan@grunenberger.net>
-RUN apk add --no-cache qemu-system-x86_64 libvirt qemu-img bridge iproute2
+RUN apk add --no-cache qemu-system-x86_64 libvirt qemu-img bridge iproute2 dnsmasq iptables
 WORKDIR /root
 COPY build_qemu/vyos.img /root/vyos.img
 COPY docker_run.sh /root/docker_run.sh
--- docker_run.sh
+++ docker_run.sh
@@ -12,9 +12,9 @@ echo "IPv4 assigned by Docker : $IPV4"
 ip link add name br0 type bridge
 
 # delete IP address
-ip addr flush dev eth0
-ip link set eth0 promisc on
-ip link set eth0 master br0
+#ip addr flush dev eth0
+# ip link set eth0 promisc on
+# ip link set eth0 master br0
 
 # create a tap0 interface for QEMU
 ip tuntap add mode tap tap0
@@ -25,9 +25,16 @@ ip link set tap0 master br0
 # raise all interface
 ip link set dev br0 up
 ip link set dev tap0 up
-ip link set dev eth0 up
+# ip link set dev eth0 up
 
-ip addr add ${IPV4} dev br0
+#ip addr add ${IPV4} dev br0
+ip addr add 192.168.99.1/30 dev br0
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 22 -j DNAT --to-destination 192.168.99.2:22
+
+dnsmasq -i br0 --dhcp-range=192.168.99.2,192.168.99.2,255.255.255.252,24h
 
 # launch QEMU
 /usr/bin/qemu-system-x86_64 -serial telnet:0.0.0.0:1234,server,nowait -device virtio-net,netdev=network0,mac=52:55:00:d1:55:01 -netdev tap,id=network0,ifname=tap0,script=no,downscript=no -m 512 -smp 1 -machine type=pc,accel=kvm -drive file=/root/vyos.img,if=virtio,cache=writeback,discard=ignore,format=qcow2 -boot once=d
